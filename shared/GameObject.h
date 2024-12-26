326ef6d3185a93e2a50e0ee93693fac1b426a058
nototria <humphrey0709@gmail.com>
2024-12-26 15:26:24 +0800

    good

--- shared/GameObject.h
+++ shared/GameObject.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <ncurses.h>
 
 #define RESET_COLOR "\033[0m"
 #define RED_COLOR "\033[31m"
@@ -10,7 +11,13 @@
 #define BLUE_COLOR "\033[34m"
 #define YELLOW_COLOR "\033[33m"
 
-enum class Direction {Right, Left, Up, Down};
+enum class Direction { Right, Left, Up, Down };
+enum class GameState {
+    TitleScreen,
+    UsernameInput,
+    GameLoop,
+    EndScreen
+};
 
 class GameObject {
 protected:
@@ -21,27 +28,44 @@ public:
     GameObject(int x = 0, int y = 0) : x(x), y(y) {}
     virtual ~GameObject() {}
 
-    // function
+    // Position setters/getters
     void setX(int newX) { x = newX; }
     void setY(int newY) { y = newY; }
 
     int getX() const { return x; }
     int getY() const { return y; }
 
-    void setPosition(int newX, int newY){
+    void setPosition(int newX, int newY) {
         x = newX;
         y = newY;
     }
+
+    // Render GameObject (can be overridden)
     virtual void render() const {
         std::cout << "Rendering GameObject at (" << x << ", " << y << ")" << std::endl;
     }
 };
 
-// Derived class: Tank
+// Tank class derived from GameObject
 class Tank : public GameObject {
 private:
     Direction direction;
     std::string color;
+
+public:
+    // Constructor
+    Tank(int x, int y, Direction dir = Direction::Up, const std::string& color = RESET_COLOR)
+        : GameObject(x, y), direction(dir), color(color) {}
+
+    // Set tank direction
+    void setDirection(Direction dir) {
+        direction = dir;
+    }
+
+    // Get tank direction
+    Direction getDirection() const { return direction; }
+
+    // Getter for direction as wide character symbol
     wchar_t getDirectionSymbol() const {
         switch (direction) {
             case Direction::Right: return L'⊢';
@@ -52,17 +76,15 @@ private:
         }
     }
 
-public:
-    Tank(int x, int y, Direction dir = Direction::Up, const std::string& color = RESET_COLOR)
-        : GameObject(x, y), direction(dir), color(color) {}
+    // Render the Tank (outside class in a separate function)
+    void render(WINDOW* win) const {
+        cchar_t renderChar;
+        renderChar.chars[0] = getDirectionSymbol();
+        renderChar.attr = A_NORMAL;
 
-    // function
-    void setDirection(Direction dir){direction = dir;}
-
-    Direction getDirection() const { return direction; }
-
-    void setColor(const std::string& newColor){color = newColor;}
-
+        mvwadd_wch(win, getY(), getX(), &renderChar);
+        wrefresh(win);
+    }
 };
 
 #endif // GAMEOBJECT_H
