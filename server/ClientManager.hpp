8fc2998d8dbbb5a9bf7d23c7b4d496492c4df0e2
CubeMan <CubeMan940331@gmail.com>
2024-12-27 15:05:10 +0800

    handle join, exit room and simple logging

--- server/ClientManager.hpp
+++ server/ClientManager.hpp
@@ -5,24 +5,28 @@
 #include<sstream>
 #include"../shared/GameParameters.h"
 #include<poll.h>
-#include<vector>
+#include<set>
 
 class ClientManager{
     struct ClientData{
         enum State{inactive, wait_id, idle, wait_start, play} state;
         std::string user_id;
-        int client_id;
+        int id;
         int room_id;
         std::stringstream ss;
     };
     struct RoomData{
-        enum State{inactive, wait, play};
-
-
+        enum State{inactive, wait, play} state;
+        std::set<int> client_id_set;
+        int host_id;
+        int id;
+        inline int player_count() const;
+        inline bool is_full() const;
     };
     char recv_buffer[1024], send_buffer[1024];
     int client_count;
     ClientData client_data_list[MAX_CLIENTS];
+    RoomData room_data_list[MAX_CLIENTS];
 public:
     struct pollfd pollfd_list[1+MAX_CLIENTS];
     ClientManager(const int&);
@@ -33,6 +37,10 @@ public:
     void recv_connection();
     void process_commands(const int);
     void listen();
+    void join_room(const int, const int);
+    void exit_room(const int, const int);
+    void start_game(const int);
+    void check_state();
 };
 
 #endif
--- shared/GameParameters.h
+++ shared/GameParameters.h
@@ -3,5 +3,8 @@
 
 #define SERV_PORT 9999
 #define MAX_CLIENTS 1000
+#define MAX_ROOMS 1000
+#define ROOM_MAX_CLIENTS 4
+#define COMMAND_SEP ','
 
 #endif
